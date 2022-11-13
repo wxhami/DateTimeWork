af6990f43e8c5891b796c8f99c61ea9d0a8f2816
wxhami <d18102002@mail.ru>
2022-11-13 21:57:45 +0300

    Task2 done

--- DateTimeWork/Program.cs
+++ DateTimeWork/Program.cs
@@ -24,5 +24,13 @@ void Task1()
 void Task2()
 {
   Console.WriteLine("Enter date your birthday");
+  DateTime dateBirthday = DateTime.Parse(Console.ReadLine());
+  DateTime today = DateTime.Now;
+  TimeSpan daysCount = today - dateBirthday;
+  Console.WriteLine($"Count days: {Math.Truncate(daysCount.TotalDays)} Count months: {Math.Truncate(daysCount.TotalDays/30)} Count years: {Math.Truncate(daysCount.TotalDays/365)} ");
+}
+
+void Task3()
+{
   
 }
