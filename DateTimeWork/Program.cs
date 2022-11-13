7a234d29e728336f45f6494c082d6a60f5c60def
wxhami <d18102002@mail.ru>
2022-11-13 21:29:37 +0300

    Task1 done

--- DateTimeWork/Program.cs
+++ DateTimeWork/Program.cs
@@ -1,1 +1,28 @@
 ﻿
+void Task1()
+{
+  Console.WriteLine("Enter your birthday: ");
+
+  DateTime dateBirthday = DateTime.Parse(Console.ReadLine());
+  DateTime dateToday = DateTime.Now;
+  DateTime date;
+  date = new DateTime(dateToday.Year, dateBirthday.Month, dateBirthday.Day);
+
+  if (date < dateToday)
+  {
+    date = date.AddYears(1);
+  }
+
+  TimeSpan countDays = date - dateToday;
+
+  Console.WriteLine($"Count days: {countDays.TotalDays}");
+
+  Console.ReadKey();
+
+}
+
+void Task2()
+{
+  Console.WriteLine("Enter date your birthday");
+  
+}
